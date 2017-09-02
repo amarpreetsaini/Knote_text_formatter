5e56fe54bccb09f89d0ad9fd8f60a926244f22da
Amarpreet Singh <amarpreetsaini@.live.com>
2017-09-01 21:38:53 -0800

    changes for retrieval

---
+++ get_table_data.py
@@ -0,0 +1,120 @@
+import cv2
+from ghostscript.ghostscript import gs_pdf_to_png
+import glob
+
+
+def create_pages(file_name):
+    gs_pdf_to_png(file_name, '300')
+    for file in glob.glob(file_name[:-4] + '*.png'):
+        all_images = [file for file in glob.glob('*.png')]
+    return all_images
+
+
+def create_coord_dict(page_path):
+    page_no = page_path[-5: -4]
+    img = cv2.imread(page_path)
+    curr_height, curr_width = img.shape[:2]
+    org_height = 792
+    org_width = 612
+    width_ratio = round(org_width / float(curr_width), 2)
+    height_ratio = round(org_height / float(curr_height), 2)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(thresh,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+    coordinates = []
+    table_coordinates = []
+
+    for index, cnt in enumerate(contours):
+        if cv2.contourArea(cnt) > 10000:
+            [point_x, point_y, width, height] = cv2.boundingRect(cnt)
+            point_x = int(point_x * width_ratio)
+            width = int(width * width_ratio)
+            point_y = int(point_y * height_ratio)
+            height = int(height * height_ratio)
+            approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+            if len(approx) >= 4:
+                if hierarchy[0][index][3] > 0:
+                    coordinates.append((point_x, point_y,
+                                        point_x + width, point_y + height))
+                elif hierarchy[0][index][3] == 0:
+                    table_coordinates.append((point_x, point_y,
+                                              point_x + width,
+                                              point_y + height))
+
+    round_off = 5
+
+    table_dict = {}
+    for tab_cord in table_coordinates:
+        for cell_coord in coordinates:
+            if tab_cord[0] < cell_coord[0] + round_off and tab_cord[1] < cell_coord[1] + round_off and tab_cord[2] > cell_coord[2] - round_off and tab_cord[3] > cell_coord[3] - round_off:
+                if tab_cord in table_dict:
+                    table_dict[tab_cord]['cell'].append(cell_coord)
+                else:
+                    table_dict.update({tab_cord: {'cell': [cell_coord]}})
+
+    table_dict_cpy = table_dict.copy()
+
+    for tab_items in table_dict_cpy.values():
+        if len(tab_items['cell']) == 1:
+            del table_dict[tab_items]
+
+    table_dict_cpy2 = table_dict.copy()
+
+    for table_key in table_dict_cpy2:
+        table = table_dict_cpy2[table_key]['cell']
+        top = [cell[1] for cell in table]
+        top = list(set(top))
+        top.sort()
+        header_top = min(list(set(top)))
+        table_dict[table_key]['header_cells'] = []
+        for cell in table:
+            if cell[1] == header_top:
+                table_dict[table_key]['header_cells'].append(cell)
+
+        left = [cell[0] for cell in table]
+        left = list(set(left))
+        left.sort()
+        left_min = min(list(set(left)))
+
+        table_dict[table_key].update({'left': left})
+
+        table_dict[table_key]['left_cells'] = []
+
+        for cell in table:
+            if cell[0] == left_min:
+                table_dict[table_key]['left_cells'].append(cell)
+        table_dict[table_key].update({'top': top})
+
+        table_dict[table_key]['columns'] = {}
+        for left_cell in left:
+            if left_cell == left_min:
+                continue
+            else:
+                i = 1
+                for cell in table:
+                    if cell[0] == left_cell:
+                        i += 1
+                        if left_cell in table_dict[table_key]['columns']:
+                            table_dict[table_key]['columns'][left_cell].append(cell)
+                        else:
+                            table_dict[table_key]['columns'].update({left_cell:[cell]})
+                            table_dict[table_key]['columns'][left_cell].sort()
+        if len(left) > 2:
+            table_type = 2
+        else:
+            table_type = 1
+
+        table_dict[table_key]['table_type'] = table_type
+        table_dict[table_key]['page_no'] = int(page_no)
+
+    return table_dict
+if __name__ == '__main__':
+    all_images = create_pages('/home/amarpreet/nlp/KnoteBinder.pdf')
+    for page_path in all_images:
+        table_cord_dict = create_coord_dict(page_path)
--- programmingChallenge.py
+++ programmingChallenge.py
@@ -1,74 +1,111 @@
 import json
 import pandas as pd
-try:
-    with open("./jsonDump.txt","r") as f:
-        jsonDump = f.read()
-except Exception as ex:
-    print "Error: Make sure jsonDump.txt exists in current directory before running"
-    exit()
-
-page_json = json.loads(jsonDump)
-coords = [(78, 710, 133, 729), (106, 711, 131, 728), (103, 539, 533, 652), (335, 637, 433, 649), (254, 637, 334, 649), (104, 637, 253, 649), (335, 623, 433, 635), (254, 623, 334, 635), (104, 623, 253, 635), (335, 594, 433, 621), (254, 594, 334, 621), (104, 594, 253, 621), (434, 569, 532, 649), (335, 569, 433, 593), (254, 569, 334, 593), (104, 569, 253, 593), (434, 540, 532, 566), (335, 540, 433, 566), (254, 540, 334, 566), (104, 540, 253, 566), (120, 130, 544, 219), (362, 190, 543, 218), (291, 190, 361, 218), (170, 190, 289, 218), (121, 190, 169, 218), (362, 160, 543, 188), (291, 160, 361, 188), (170, 160, 289, 188), (121, 160, 169, 188), (362, 131, 543, 159), (291, 131, 361, 159), (170, 131, 289, 159), (121, 131, 169, 159), (101, 71, 144, 110), (104, 67, 146, 106)]
-
-table_cord_dict = {(103, 539, 533, 652): {'header_cells': [(434, 540, 532, 566), (335, 540, 433, 566), (254, 540, 334, 566), (104, 540, 253, 566)], 'top': [540, 569, 594, 623, 637], 'cell': [(335, 637, 433, 649), (254, 637, 334, 649), (104, 637, 253, 649), (335, 623, 433, 635), (254, 623, 334, 635), (104, 623, 253, 635), (335, 594, 433, 621), (254, 594, 334, 621), (104, 594, 253, 621), (434, 569, 532, 649), (335, 569, 433, 593), (254, 569, 334, 593), (104, 569, 253, 593), (434, 540, 532, 566), (335, 540, 433, 566), (254, 540, 334, 566), (104, 540, 253, 566)], 'left_cells': [(104, 637, 253, 649), (104, 623, 253, 635), (104, 594, 253, 621), (104, 569, 253, 593), (104, 540, 253, 566)], 'page_no': 1, 'table_type': 2, 'columns': {434: [(434, 569, 532, 649), (434, 540, 532, 566)], 254: [(254, 637, 334, 649), (254, 623, 334, 635), (254, 594, 334, 621), (254, 569, 334, 593), (254, 540, 334, 566)], 335: [(335, 637, 433, 649), (335, 623, 433, 635), (335, 594, 433, 621), (335, 569, 433, 593), (335, 540, 433, 566)]}, 'left': [104, 254, 335, 434]}, (120, 130, 544, 219): {'header_cells': [(362, 131, 543, 159), (291, 131, 361, 159), (170, 131, 289, 159), (121, 131, 169, 159)], 'top': [131, 160, 190, 199], 'cell': [(362, 190, 543, 218), (369, 199, 533, 210), (291, 190, 361, 218), (170, 190, 289, 218), (121, 190, 169, 218), (362, 160, 543, 188), (291, 160, 361, 188), (170, 160, 289, 188), (121, 160, 169, 188), (362, 131, 543, 159), (291, 131, 361, 159), (170, 131, 289, 159), (121, 131, 169, 159)], 'left_cells': [(121, 190, 169, 218), (121, 160, 169, 188), (121, 131, 169, 159)], 'page_no': 1, 'table_type': 2, 'columns': {369: [(369, 199, 533, 210)], 170: [(170, 190, 289, 218), (170, 160, 289, 188), (170, 131, 289, 159)], 291: [(291, 190, 361, 218), (291, 160, 361, 188), (291, 131, 361, 159)], 362: [(362, 190, 543, 218), (362, 160, 543, 188), (362, 131, 543, 159)]}, 'left': [121, 170, 291, 362, 369]}}
+from get_table_data import create_pages
+from get_table_data import create_coord_dict
 
 
+def main(table_cord_dict):
+    try:
+        with open("./jsonDump.txt","r") as f:
+            jsonDump = f.read()
+    except Exception as ex:
+        print "Error: Make sure jsonDump.txt exists in current directory before running"
+        exit()
 
-matrix = {}
-for page_no, page in enumerate(page_json, start=1):
-    #import pdb;pdb.set_trace()
-    print page_no
-    for table_key in table_cord_dict:
-        if page_no == table_cord_dict[table_key]['page_no']:
-            table_values = table_cord_dict[table_key]
-            table = table_values['cell']
-            left = table_values['left']
-            top = table_values['top']
-            skip_table = False
+    page_json = json.loads(jsonDump)
 
-            round_off = 5
-            matrix[table_key] = {}
-            for child in page['blocks']:
-                for line in child['lines']:
-                    for span in line['spans']:
-                        #we encode to ascii to ignore strange unicode characters.
-                        #if you need them for some purpose, you can remove the
-                        #'.encode()' call and replace it.
-                        current_text = span['text'].encode('ascii',errors='ignore')
-                        current_bbox = span['bbox']
-                        for coord in table:
-                            if current_bbox[0] > (coord[0] - round_off) and current_bbox[1] > (coord[1] - round_off) and current_bbox[2] < (coord[2] + round_off) and current_bbox[3] < (coord[3] + round_off):
-                                x, y = 0, 0
-                                for l in left:
-                                    if l == coord[0]: y = left.index(l)
-                                for t in top:
-                                    if t == coord[1]: x = top.index(t)
+    matrix = {}
+    for page_no, page in enumerate(page_json, start=1):
+        for table_key in table_cord_dict:
+            if page_no == table_cord_dict[table_key]['page_no']:
+                table_values = table_cord_dict[table_key]
+                table = table_values['cell']
+                left = table_values['left']
+                top = table_values['top']
+                skip_table = False
 
-                                t = x, y
-                                if t in matrix[table_key]:
-                                    matrix[table_key][t] += current_text
-                                else:
-                                    matrix[table_key][t] = current_text
+                round_off = 5
+                matrix[table_key] = {}
+                for child in page['blocks']:
+                    for line in child['lines']:
+                        for span in line['spans']:
+                            current_text = span['text'].encode('ascii',errors='ignore')
+                            current_bbox = span['bbox']
+                            for coord in table:
+                                if current_bbox[0] > (coord[0] - round_off) and current_bbox[1] > (coord[1] - round_off) and current_bbox[2] < (coord[2] + round_off) and current_bbox[3] < (coord[3] + round_off):
+                                    x, y = 0, 0
+                                    for l in left:
+                                        if l == coord[0]: y = left.index(l)
+                                    for t in top:
+                                        if t == coord[1]: x = top.index(t)
 
-                                    if matrix[table_key][t] == "VIA: ":
-                                        skip_table = True
+                                    t = x, y
+                                    if t in matrix[table_key]:
+                                        matrix[table_key][t] += current_text
+                                    else:
+                                        matrix[table_key][t] = current_text
 
-            final_dict = {}
-            if table_cord_dict[table_key]['table_type'] == 2:
-                print skip_table
-                for elem in matrix[table_key]:
-                    if skip_table:
-                        continue
-                    else:
-                        if elem[0] > 0:
-                            if matrix[table_key][(elem[0],0)] not in final_dict:
-                                final_dict[matrix[table_key][(elem[0],0)]] = {}
-                        for elem2 in matrix[table_key]:
-                            if elem[0] > 0 and elem[1] == 0 and elem2[0] == 0 and elem2[1] > 0:
-                                try:
-                                    final_dict[matrix[table_key][(elem[0],0)]][matrix[table_key][(0,elem2[1])]] = matrix[table_key][(elem[0], elem2[1])]
-                                except Exception as exc:
-                                    pass
+                                        if matrix[table_key][t] in ["VIA: ", "Endorsements "]:
+                                            skip_table = True
                 if not skip_table:
-                    df = pd.DataFrame.from_dict(final_dict, orient='index').fillna(method='ffill')
-                    print df
+                    if table_cord_dict[table_key]['table_type'] == 2:
+                        df = create_2d_data_frame(matrix, skip_table, table_key)
+                        result_dict = {}
+                        for elem in matrix[table_key]:
+                            if elem[1] == 0 and elem[0] > 0:
+                                result_dict[matrix[table_key][(elem[0],0)]] = {}
+                            for elem2 in matrix[table_key]:
+                                if elem[1] == 0 and elem[0] > 0 and elem2[0] == 0 and elem2[1] >0:
+                                    result_dict[matrix[table_key][(elem[0],0)]].update({ matrix[table_key][(0,elem2[1])]: df[matrix[table_key][elem2]][matrix[table_key][elem]]})
+
+                        print result_dict
+                    else:
+                        final_dict = create_1d_data_frame(matrix, skip_table, table_key)
+                        print final_dict
+
+
+def create_1d_data_frame(matrix, skip_table, table_key):
+    if skip_table:
+        return None
+    else:
+        final_dict = {}
+        final_dict[matrix[table_key][(0, 0)]] = {}
+        for elem in matrix[table_key]:
+            for elem2 in matrix[table_key]:
+                if ':' in matrix[table_key][(0, 0)]:
+                    if elem[0] > 0 and elem[1] == 0 and elem2[1] == 1 and elem[0] == elem2[0]:
+                        final_dict[matrix[table_key][(0, 0)]].update({matrix[table_key][elem]: matrix[table_key][(elem[0], 1)]})
+                else:
+                    if elem[1] == 0 and elem2[1] == 1 and elem[0] == elem2[0]:
+                        final_dict[matrix[table_key][elem]] =\
+                            matrix[table_key][(elem[0], 1)]
+
+    return final_dict
+
+
+def create_2d_data_frame(matrix, skip_table, table_key):
+    df = None
+    if skip_table:
+        return None
+    else:
+        final_dict = {}
+        for elem in matrix[table_key]:
+            if elem[1] == 0 and elem[0] > 0:
+                final_dict[matrix[table_key][(elem[0],0)]] = {}
+            for elem2 in matrix[table_key]:
+                if elem[0] > 0 and elem[1] == 0 and elem2[0] == 0 and elem2[1] > 0:
+                    try:
+                        final_dict[matrix[table_key][(elem[0],0)]][matrix[table_key][(0,elem2[1])]] = matrix[table_key][(elem[0], elem2[1])]
+                    except Exception as exc:
+                        pass
+    if not skip_table:
+        df = pd.DataFrame.from_dict(final_dict, orient='index').fillna(method='ffill')
+    return df
+
+
+if __name__ == '__main__':
+    all_images = create_pages('KnoteBinder.pdf')
+    for page_path in all_images:
+        table_cord_dict = create_coord_dict(page_path)
+        if table_cord_dict:
+            main(table_cord_dict)
